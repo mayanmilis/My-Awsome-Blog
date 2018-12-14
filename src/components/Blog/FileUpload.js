d8709e769db4380d0e634ff407e36e01077b60e5
mayanmilis <mayanmilis16@gmail.com>
2018-12-14 17:59:19 +0200

    added photos to blog details

--- src/components/Blog/FileUpload.js
+++ src/components/Blog/FileUpload.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import React, { Component } from 'react'
 // import classes from './CreateBlog.css'
 // import { storage } from '../fbConfig/fbConfig'
@@ -70,69 +69,3 @@
 //         {collection: 'files'}
 //     ])
 //     )(FileUpload)
-=======
-import React, { Component } from 'react'
-import classes from './CreateBlog.css'
-import { storage } from '../fbConfig/fbConfig'
-import { connect } from 'react-redux'
-import { getFirestore } from 'redux-firestore';
-import { uploadFile } from '../Store/Actions/BlogActions'
-import { compose } from 'redux'
-import { firestoreConnect } from 'react-redux-firebase'
-
-
-class FileUpload extends Component {
-    // state = {
-    //     selectedFile: null,
-    //     url: ''
-    // }
-    state = {
-        selectedFile: null,
-    }
-
-     selectFile = (event) => {
-         this.setState({
-             selectedFile: event.target.files[0]
-         })
-        
-    }
-
-    uploadFile = (event) => {
-        event.preventDefault();
-        console.log(this.state.selectedFile.name)
-        this.props.uploadFile(this.state.selectedFile)
-    }
-
-    render() {
-        const {files} = this.props;
-        return(
-            <div className={classes.FileUpload}>
-                <h5>Upload Photos</h5>
-                <input type="file" onChange={this.selectFile}/>
-                <button onClick={this.uploadFile}>upload</button>
-                <p>{this.state.url}</p>
-            </div>
-        )
-    }
-}
-
-const mapDispatchToProps = (dispatch) =>{
-    return{
-        uploadFile: (file) => dispatch(uploadFile(file))
-    }
-}
-
-const mapStateToProps = (state) => {
-    console.log(state)
-    return{
-        files: state.firestore.ordered.files
-    }
-}
-
-export default compose(
-    connect(mapStateToProps, mapDispatchToProps),
-    firestoreConnect([
-        {collection: 'files'}
-    ])
-    )(FileUpload)
->>>>>>> a4d3136cc4863e0dedf1e6ec5f831b9c5cda83a3
--- src/components/Store/Reducers/BlogReducer.js
+++ src/components/Store/Reducers/BlogReducer.js
@@ -1,32 +1,24 @@
 const initialState = {
-blogs: []
-}
-
-const blogReducer = (state = initialState, action) => {
-    switch(action.type){
-        case 'CREATE_BLOG':
-        console.log('created blog', action.blog)
-        return state;
-        case 'CREATE_BLOG_ERROR':
-        console.log('create blog error', action.err)
-        return state;
-<<<<<<< HEAD
-        // case 'Upload_SUCCESS':
-        // console.log('upload_success', action.err)
-        // return state;
-        // case 'Upload_ERROR':
-        // console.log('upload_error', action.err)
-=======
-        case 'UPLOAD_SUCCESS':
-        console.log('upload_success', action.err)
-        return state;
-        case 'UPLOAD_ERROR':
-        console.log('upload_error', action.err)
->>>>>>> a4d3136cc4863e0dedf1e6ec5f831b9c5cda83a3
-        return state;
-        default:
-        return state;
+    blogs: []
     }
-}
-
-export default blogReducer
+    
+    const blogReducer = (state = initialState, action) => {
+        switch(action.type){
+            case 'CREATE_BLOG':
+            console.log('created blog', action.blog)
+            return state;
+            case 'CREATE_BLOG_ERROR':
+            console.log('create blog error', action.err)
+            return state;
+            // case 'Upload_SUCCESS':
+            // console.log('upload_success', action.err)
+            // return state;
+            // case 'Upload_ERROR':
+            // console.log('upload_error', action.err)
+            // return state;
+            default:
+            return state;
+        }
+    }
+    
+    export default blogReducer
